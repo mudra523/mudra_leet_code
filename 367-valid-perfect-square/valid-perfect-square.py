93ed196b43b99510c392f7662b71c705b666cc80
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-10-24 16:04:51 -0400

    Time: 7 ms (93.49%) | Memory: 16.5 MB (28.44%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        # O(sqrt(n)):
+        for i in range(1, num + 1):
+            if i * i == num:
+                return True
+            if i * i > num:
+                return False
+        
+        

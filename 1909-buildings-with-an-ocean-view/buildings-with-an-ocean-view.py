bbf8bc4301694690e9bc7507d9eb88b184373e03
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-10-26 18:30:40 -0400

    Time: 58 ms (22.40%) | Memory: 30.2 MB (90.59%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,5 +1,7 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
+        #TC O(n)
+        #SC O(n)
         n = len(heights)
         res = []
         for i in range(n):

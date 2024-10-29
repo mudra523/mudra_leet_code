6f518d84204be1745db340a018fed76896688306
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-10-28 22:32:43 -0400

    Time: 0 ms (100.00%) | Memory: 20.1 MB (24.28%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -1,23 +1,32 @@
 class MovingAverage:
-
-#     def __init__(self, size: int):
-#         self
-        
-
-#     def next(self, val: int) -> float:
-        
-    
-    # Solution 1 TC: O(N.M), SC: O(M)
+    # Approach 2: Array or List
+    # Solution 2 TC: O(1), SC: O(N) where N is the size of the moving window.
     def __init__(self, size: int):
         self.size = size
-        self.queue = []
+        self.queue = deque()
+        self.window_sum = 0
+        self.count = 0
 
     def next(self, val: int) -> float:
-        size, queue = self.size, self.queue
-        queue.append(val)
+        self.count += 1
+        self.queue.append(val)
         
-        window_sum = sum(queue[-size:])
-        return window_sum / min(len(queue), size)
+        tail = self.queue.popleft() if self.count > self.size else 0
+        self.window_sum = self.window_sum + val - tail
+        return self.window_sum / min(self.count, self.size)
+    
+    # Approach 1: Array or List
+    # Solution 1 TC: O(N.M), SC: O(M)
+#     def __init__(self, size: int):
+#         self.size = size
+#         self.queue = []
+
+#     def next(self, val: int) -> float:
+#         size, queue = self.size, self.queue
+#         queue.append(val)
+        
+#         window_sum = sum(queue[-size:])
+#         return window_sum / min(len(queue), size)
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)

14de01a7146c03f489af2eefc8f217d6a717a412
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-11-04 19:09:00 -0500

    Time: 0 ms (100.00%) | Memory: 16.8 MB (9.09%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        #Solution 1: TC: O(n) SC O(1)
+        # for i in range(len(nums) - 1):
+        #     if nums[i] > nums[i + 1]:
+        #         return i
+        # return len(nums) - 1
+    
+        #Solution 2: TC: O(log(n)) SC O(1)
+       
+        return self.search(nums, 0, len(nums) - 1)
+
+    def search(self, nums: List[int], l: int, r: int) -> int:
+        if l == r:
+            return l
+        mid = (l + r) // 2
+        if nums[mid] > nums[mid + 1]:
+            return self.search(nums, l, mid)
+        return self.search(nums, mid + 1, r)

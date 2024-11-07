f6bca33d36b66e90b08b7a662fb1531583631232
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-11-06 20:56:43 -0500

    Time: 0 ms (100.00%) | Memory: 16.8 MB (40.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -7,22 +7,22 @@ class Solution:
         # return len(nums) - 1
     
         #Solution 2: TC: O(log(n)) SC O(1)
-        return self.search(nums, 0, len(nums) - 1)
-    def search(self, nums: List[int], l: int, r: int) -> int:
-        if l == r:
-            return l
-        mid = (l + r) // 2
-        if nums[mid] > nums[mid + 1]:
-            return self.search(nums, l, mid)
-        return self.search(nums, mid + 1, r)
+    #     return self.search(nums, 0, len(nums) - 1)
+    # def search(self, nums: List[int], l: int, r: int) -> int:
+    #     if l == r:
+    #         return l
+    #     mid = (l + r) // 2
+    #     if nums[mid] > nums[mid + 1]:
+    #         return self.search(nums, l, mid)
+    #     return self.search(nums, mid + 1, r)
         
         #Solution 3: TC: O(log(n)) SC O(1)
-        # l = 0
-        # r = len(nums) - 1
-        # while l < r:
-        #     mid = (l + r) // 2
-        #     if nums[mid] > nums[mid + 1]:
-        #         r = mid
-        #     else:
-        #         l = mid + 1
-        # return l
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+        return l

cbe6f8d0092036f6b31819deaccc43dfa517c72e
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-11-06 19:41:58 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (76.13%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,19 +1,29 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         #Solution 1: TC: O(n) SC O(1)
-        # for i in range(len(nums) - 1):
-        #     if nums[i] > nums[i + 1]:
-        #         return i
-        # return len(nums) - 1
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                return i
+        return len(nums) - 1
     
         #Solution 2: TC: O(log(n)) SC O(1)
-       
-        return self.search(nums, 0, len(nums) - 1)
+#         return self.search(nums, 0, len(nums) - 1)
 
-    def search(self, nums: List[int], l: int, r: int) -> int:
-        if l == r:
-            return l
-        mid = (l + r) // 2
-        if nums[mid] > nums[mid + 1]:
-            return self.search(nums, l, mid)
-        return self.search(nums, mid + 1, r)
+#     def search(self, nums: List[int], l: int, r: int) -> int:
+#         if l == r:
+#             return l
+#         mid = (l + r) // 2
+#         if nums[mid] > nums[mid + 1]:
+#             return self.search(nums, l, mid)
+#         return self.search(nums, mid + 1, r)
+
+        #Solution 3: TC: O(log(n)) SC O(1)
+        # l = 0
+        # r = len(nums) - 1
+        # while l < r:
+        #     mid = (l + r) // 2
+        #     if nums[mid] > nums[mid + 1]:
+        #         r = mid
+        #     else:
+        #         l = mid + 1
+        # return l

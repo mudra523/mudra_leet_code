f799caa97b51e7d2aaea51ac3730e82641a88206
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-11-24 17:57:28 -0500

    Time: 15 ms (96.95%) | Memory: 22.4 MB (68.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,19 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+        # Solution 1
+        # n = len(nums)
+        # pre = [1] * n
+        # suf = [1] * n
         
-        # Initialize arrays to store prefix and suffix products
-        pre = [1] * n
-        suf = [1] * n
-        
-        for i in range(1, n):
-            pre[i] = pre[i - 1] * nums[i - 1]
+        # for i in range(1, n):
+        #     pre[i] = pre[i - 1] * nums[i - 1]
 
-        for i in range(n - 2, -1, -1):
-            suf[i] = suf[i + 1] * nums[i + 1]
+        # for i in range(n - 2, -1, -1):
+        #     suf[i] = suf[i + 1] * nums[i + 1]
         
-        res = [0] * n
+        # res = [0] * n
+        # for i in range(n):
+        #     res[i] = pre[i] * suf[i]
+        # return res
+        
+        n = len(nums)
+        res = [1] * n
+        
+        pre = 1
         for i in range(n):
-            res[i] = pre[i] * suf[i]
-        return res
+            res[i] = pre
+            pre *= nums[i]
         
+        suf = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= suf
+            suf *= nums[i]
+        
+        return res

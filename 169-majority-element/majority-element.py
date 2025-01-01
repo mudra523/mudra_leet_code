06dacc2a7a8d88bece64023ea482a0a2ec136b95
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2024-12-31 19:52:10 -0500

    Time: 8 ms (40.37%) | Memory: 19.3 MB (19.33%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,16 +2,22 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Boyer Moore Algoritham
         # Space Complexity is O(1)
-        
+        count, res = 0, 0
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+        return res
+
 
 
 
         # SC O(n)
-        count = {}
-        res, maxCount = 0, 0
+        # count = {}
+        # res, maxCount = 0, 0
 
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-            res = n if count[n] > maxCount else res
-            maxCount = max(count[n], maxCount)
-        return res
+        # for n in nums:
+        #     count[n] = 1 + count.get(n, 0)
+        #     res = n if count[n] > maxCount else res
+        #     maxCount = max(count[n], maxCount)
+        # return res

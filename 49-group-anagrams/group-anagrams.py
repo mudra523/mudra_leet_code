887e9e3f1ffc9b900c7bcfcb5d2a4cb3dec65f89
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2025-01-11 01:19:27 -0500

    Time: 16 ms (44.21%) | Memory: 22.6 MB (15.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,26 +1,25 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
         # Solution 3 TC = O(n)
         hasMap = defaultdict(list)
         for s in strs:
-            count= [0] * 26
+            count = [0] * 26
             for c in s:
-                print("C", c)
                 count[ord(c) - ord('a')] += 1
             hasMap[tuple(count)].append(s)
         return list(hasMap.values())
 
 
         # Solution 2 TC: O(k⋅nlogn) SC: O(\U0001d458⋅\U0001d45b)
-        # hasMap = {}
-
-        # for s in strs:
-        #     sorted_s = ''.join(sorted(s))
-        #     if sorted_s not in hasMap:
-        #         hasMap[sorted_s] = []
-        #     hasMap[sorted_s].append(s)
-        # return list(hasMap.values())
+        # anagrams = defaultdict(list)
+        # for word in strs:
+        #     # Sort the word and use it as the key
+        #     sorted_word = ''.join(sorted(word))
+        #     anagrams[sorted_word].append(word)
+        # # Convert the dictionary values to a list
+        # return list(anagrams.values())
        
         # Solution 1. TC: O(L) SC: O(L)
         # def createKey(word):

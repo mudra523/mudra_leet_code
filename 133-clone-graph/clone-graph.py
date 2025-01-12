092bcaa4817bfdea68b695c98864fc85ed28eb76
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2025-01-12 18:07:07 -0500

    Time: 42 ms (54.93%) | Memory: 18.1 MB (34.56%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -5,7 +5,11 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+        
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         oldToNew = {}
@@ -16,9 +20,11 @@ class Solution:
 
             copy = Node(node.val)
             oldToNew[node] = copy
-            for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
+            for n in node.neighbors:
+                copy.neighbors.append(dfs(n))
             return copy
         return dfs(node) if node else None
+
+
 
         

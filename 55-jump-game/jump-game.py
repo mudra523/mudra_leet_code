f28b6286335a70174c9304fe099d33171b3e4e45
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2025-01-22 22:20:52 -0500

    Time: 15 ms (76.02%) | Memory: 18.9 MB (14.75%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,8 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        #TC O(n) SC O(1)
         target = len(nums) - 1
 
-        for i in range(len(nums) - 1, -1, -1):
+        for i  in range(len(nums) - 1, -1, -1):
             if nums[i] + i >= target:
                 target = i
         return True if target == 0 else False

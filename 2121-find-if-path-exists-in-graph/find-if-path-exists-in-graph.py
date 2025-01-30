207b4afab3ab8264abe984beb7c7c5c2d7ea0781
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2025-01-30 14:12:40 -0500

    Time: 315 ms (86.29%) | Memory: 96.6 MB (65.30%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,5 +1,32 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        #1. DFS with Recursion
+        # if source == destination:
+        #     return True
+
+        # graph = defaultdict(list)
+        
+        # for u, v in edges:
+        #     graph[u].append(v)
+        #     graph[v].append(u)
+
+        # seen = set()
+        # seen.add(source)
+
+        # def dfs(i):
+        #     if i == destination:
+        #         return True
+            
+        #     for nei_node in graph[i]:
+        #         if nei_node not in seen:
+        #             seen.add(nei_node)
+        #             if dfs(nei_node):
+        #                 return True
+        #     return False
+
+        # return dfs(source)
+
+        #2. DFS with Stack:
         if source == destination:
             return True
 
@@ -11,16 +38,15 @@ class Solution:
 
         seen = set()
         seen.add(source)
+        stack = [source]
 
-        def dfs(i):
-            if i == destination:
+        while stack:
+            node = stack.pop()
+            if node == destination:
                 return True
-            
-            for nei_node in graph[i]:
-                if nei_node not in seen:
-                    seen.add(nei_node)
-                    if dfs(nei_node):
-                        return True
-            return False
 
-        return dfs(source)
+            for i in graph[node]:
+                if i not in seen:
+                    seen.add(i)
+                    stack.append(i)
+        return False
